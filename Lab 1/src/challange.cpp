bbb643078b29288341de8967cb044956a9347120
thoang12345 <thienanhoang@student.midlandstech.edu>
2026-08-28 14:08:29 -0400

    updated code

--- Lab 1/src/challange.cpp
+++ Lab 1/src/challange.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
-const int button_A = 0;
-const int button_B = 1;
+const int button_A = A0;
+const int button_B = A1;
 
 int button_A_state = 0;
 int button_B_state = 0;
@@ -18,6 +18,7 @@ unsigned long last_Change_Time = 0;
 int green_Direction = 0;  // 0 = A is green, 1 = B is green
 int phase = 0;           // 0 = normal, 1 = yellow, 2 = both red
 
+//these helper functions exist to set the light combonations
 void direction_a_green() {
   digitalWrite(a_Green, HIGH);
   digitalWrite(a_Yellow, LOW);
@@ -90,19 +91,24 @@ void loop() {
   button_A_state = digitalRead(button_A);
   button_B_state = digitalRead(button_B);
 
-  Serial.print("A: ");
+  /*Serial.print("A: ");
   Serial.print(button_A_state);
   Serial.print("  B: ");
-  Serial.println(button_B_state);
+  Serial.println(button_B_state);*/
 
   unsigned long current_Time = millis();
 
   // Normal: one direction is green
+  //If a is green only button b can work
+  //if b is green only button a can work
   if (phase == 0) {
     bool car_At_Red_Light =
-      (green_Direction == 0 && button_B_state == HIGH) ||
+      (green_Direction == 0 && button_B_state == HIGH) || 
       (green_Direction == 1 && button_A_state == HIGH);
 
+    //cool down timers to check if the green light has been 
+    //for 10 seconds or if a car has been waiting for at least
+    //3 seconds
     bool ten_Seconds_Passed =
       current_Time - last_Change_Time >= 10000;
 
@@ -122,6 +128,9 @@ void loop() {
   }
 
   // Yellow has been on for one second
+  //it will make the light yellow for one second and then
+  //it calls the both_red helper function to make 
+  //lights red
   else if (phase == 1) {
     if (current_Time - last_Change_Time >= 1000) {
       both_red();
@@ -131,6 +140,9 @@ void loop() {
   }
 
   // Both directions have been red for two seconds
+  //this will make sure that both lights are both red
+  //for at least 2 seconds. Then it will give the green to the other
+  //direction. If a was green, b is now green and vice versa
   else if (phase == 2) {
     if (current_Time - last_Change_Time >= 2000) {
       if (green_Direction == 0) {
